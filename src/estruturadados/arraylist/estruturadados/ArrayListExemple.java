a1bff33360b9a41722be078edfa4510efd622bcd
Bruno <brunobilheri@hotmail.com>
2021-12-27 09:59:52 -0300

    Adding arrayList exemple

--- src/estruturadados/arraylist/estruturadados/ArrayListExemple.java
+++ src/estruturadados/arraylist/estruturadados/ArrayListExemple.java
@@ -1,4 +1,26 @@
 package estruturadados.arraylist.estruturadados;
 
+import java.util.ArrayList;
+
 public class ArrayListExemple {
+    public static void main(String[] args) {
+
+        ArrayList<String> arrayList = new ArrayList<String>();
+
+        arrayList.add("A");
+        arrayList.add("B");
+        arrayList.add("C");
+
+        System.out.println(arrayList);
+
+        // adicionando na posição 1
+        arrayList.add(0, "D");
+
+        System.out.println(arrayList);
+
+        // removendo pela posição do array
+        arrayList.remove(0);
+
+        System.out.println(arrayList);
+    }
 }
