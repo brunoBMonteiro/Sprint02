4cb5cc15855afad30a181bbe4a50cb715a7e59c8
Bruno <brunobilheri@hotmail.com>
2022-01-05 11:10:51 -0300

    Declaração do switch

---
+++ src/Objects/ObjectsJavaUtil.java
@@ -0,0 +1,9 @@
+package Objects;
+/*
+O java. util. A classe de objetos faz parte do Java desde a versão 1.7.
+Esta classe fornece métodos de utilitário estáticos para objetos que podem ser
+usados para realizar algumas das tarefas diárias, como verificação de igualdade, verificação de nulos, etc.
+ */
+
+
+
