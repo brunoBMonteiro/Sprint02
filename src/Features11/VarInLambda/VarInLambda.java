670484302102cb63659c5fc9e901a8ebf3e4ce29
Bruno <brunobilheri@hotmail.com>
2022-01-05 08:52:38 -0300

    Var in lambda expression

---
+++ src/Features11/VarInLambda/VarInLambda.java
@@ -0,0 +1,16 @@
+package Features11.VarInLambda;
+
+import java.util.List;
+
+public class VarInLambda {
+
+    public static void main(String[] args) {
+
+        List<Integer> lista = List.of(1,2,3,4);
+
+        lista.forEach(
+                (var e) -> System.out.println(e)
+        );
+
+    }
+}
