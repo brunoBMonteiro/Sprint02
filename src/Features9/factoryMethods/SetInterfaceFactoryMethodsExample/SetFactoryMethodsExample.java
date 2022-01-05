7099d7bbc437e3bda075e113a8a7ea42681d86b8
Bruno <brunobilheri@hotmail.com>
2022-01-04 22:22:44 -0300

    Factory methods example

---
+++ src/Features9/factoryMethods/SetInterfaceFactoryMethodsExample/SetFactoryMethodsExample.java
@@ -0,0 +1,12 @@
+package Features9.factoryMethods.SetInterfaceFactoryMethodsExample;
+
+import java.util.Set;
+
+public class SetFactoryMethodsExample {
+    public static void main(String[] args) {
+        Set<String> set = Set.of("Java","JavaFX","Spring","Hibernate","JSP");
+        for(String l:set) {
+            System.out.println(l);
+        }
+    }
+}
