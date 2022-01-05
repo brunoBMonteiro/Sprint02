af3f779b141c479a76b76f924e4901b13f9bb92e
Bruno <brunobilheri@hotmail.com>
2022-01-05 11:25:07 -0300

    Correspondencia de padrão

--- src/Features17/PatterMatchingForSwith/DeclaraçãoSwitch/SwitchTeste.java
+++ src/Features17/PatterMatchingForSwith/DeclaraçãoSwitch/SwitchTeste.java
@@ -5,12 +5,15 @@ package Features17.PatterMatchingForSwith.DeclaraçãoSwitch;
 public class SwitchTeste {
     public static void main(String[] args) {
 
+        /*
         final String b = "B";
         switch (args[0]) {
             case "A" -> System.out.println("Parameter is A");
             case b -> System.out.println("Parameter is b");
             default -> System.out.println("Parameter is unknown");
         };
+
+         */
         // Em nosso exemplo, se a variável B não fosse final,
         // o compilador lançaria uma expressão constante exigida de erro.
     }
