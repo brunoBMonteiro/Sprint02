3b602b79068c5cef854a7a3197eee37da189aaef
Bruno <brunobilheri@hotmail.com>
2022-01-05 12:29:31 -0300

    Finished

--- src/Objects/ObjectsJavaUtil.java
+++
@@ -1,9 +0,0 @@
-package Objects;
-/*
-O java. util. A classe de objetos faz parte do Java desde a versão 1.7.
-Esta classe fornece métodos de utilitário estáticos para objetos que podem ser
-usados para realizar algumas das tarefas diárias, como verificação de igualdade, verificação de nulos, etc.
- */
-
-
-
